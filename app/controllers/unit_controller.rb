433a9092e03830187efad6d569fdd16e498b2364
benhayehudi <bengreenberg@gmail.com>
2019-10-30 15:47:30 +0200

    working towards moving call towards menu after authentication

--- app/controllers/unit_controller.rb
+++ app/controllers/unit_controller.rb
@@ -3,67 +3,104 @@ require "ibm_watson/speech_to_text_v1"
 require 'json'
 class UnitController < ActionController::Base
   skip_before_action :verify_authenticity_token
+  layout false
 
   def answer
     render json:
     [
-      {
-        :action => 'conversation',
-        :name => 'sensibo-conversation'
-      },
       { 
         :action => 'talk', 
         :text => welcome_text
       },
       {
         :action => 'record',
-        :eventUrl => ["#{ENV['BASE_URL']}/authenticate"],
+        :eventUrl => ["#{ENV['BASE_URL']}/record"],
         :format => 'wav',
         :beepStart => true,
         :endOnKey => '#'
+      },
+      {
+        :action => 'talk',
+        :text => 'Please wait a moment as your passphrase is verified.'
+      },
+      {
+        :action => 'conversation',
+        :name => 'sensibo-conversation',
+        :eventUrl => ["#{ENV['BASE_URL']}/event"]
       }
     ].to_json
   end
 
   def event
-    puts params
+    if params['type'] == 'transfer'
+      if is_authenticated?('passphrase.wav')
+        render json:
+        {
+          :type => 'ncco',
+          :ncco => [
+          {
+            :action => 'talk',
+            :text => menu_options_text
+          },
+          {
+            :action => 'input',
+            :eventUrl => "#{ENV['BASE_URL']}/menu-choice",
+            :bargeIn => true,
+            :endOnKey => '#'
+          }
+        ]}.to_json
+        puts "AUTHENTICATED!!!"
+      else
+        render json: 
+        {
+          :type => 'ncco',
+          :ncco => [
+          {
+            :action => 'talk',
+            :text => 'Sorry your passphrase did not match. Please call back and try again.'
+          }
+        ]}.to_json
+        puts "NOT AUTHENTICATED!!!"
+      end
+    else
+      puts "SKIPPED OVER IS_AUTHENTICATED? METHOD"
+    end
   end
 
-  def authenticate
-    Nexmo.files.save(params['recording_url'], 'passphrase.wav')
-    @audio_file = 'passphrase.wav'
-    if is_authenticated?(@audio_file)
-      render json: 
-      [
-        {
-          :action => 'talk',
-          :text => menu_options_text
-        },
-        {
-          :action => 'input',
-          :eventUrl => "#{ENV['BASE_URL']}/menu-choice"
-          :bargeIn => true,
-          :endOnKey => '#'
-        }
-      ]
+  def record
+    if params['recording_url']
+      file = Nexmo.files.save(params['recording_url'], 'passphrase.wav')
+    end
+  end
+      
+  def menu
+    case params['dtmf']
+    when '1'
+      response = get_sensibo_status("#{ENV['SENSIBO_ID']}")
+      byebug
+    when '2'
+      #action = update_sensibo_status('on')
+    when '3'
+      #action = update_sensibo_status('off')
     else
       render json: 
       [
         {
-          :action => 'talk',
-          :text => 'Sorry your passphrase did not match. Please call back and try again.'
+          :action => 'talk', 
+          :text => "You entered an incorrect choice of #{params['dtmf']}. Expected 1, 2 or 3. Please try again."
+        },
+        {
+          :action => 'connect',
+          :eventUrl => "#{ENV['BASE_URL']}/authenticate"
         }
       ]
     end
   end
 
-  def menu
-  end
-
   private
 
   def is_authenticated?(audio_file)
-    transcribed = ibm_speech_to_text(@audio_file)
+    transcribed = ibm_speech_to_text(audio_file)
     ENV['SECRET_WORD'] == transcribed
   end
 
@@ -77,18 +114,31 @@ class UnitController < ActionController::Base
       recognition = speech_to_text.recognize(
         audio: audio_file,
         content_type: "audio/wav",
-        timestamps: true,
-        word_confidence: true,
         model: 'en-US_BroadbandModel'
       ).result
     end
-    recognition['results']['transcript']
+    recognition['results'][0]['alternatives'][0]['transcript'].strip
   end
 
   def ibm_authenticator
     authenticator = IBMWatson::Authenticators::IamAuthenticator.new(
       apikey: "#{ENV['WATSON_API_KEY']}"
     )
+  end
+
+  def get_sensibo_status(id)
+    require 'net/https'
+
+    begin
+        uri = URI("#{ENV['SENSIBO_API_URL']}/#{id}")
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        req = Net::HTTP::Get.new(uri.path, {'Content-Type' => 'application/json'})
+        res = http.request(req)
+        puts JSON.parse(res.body)
+    rescue => e
+        puts "failed #{e}"
+    end
   end
 
   def welcome_text
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'unit#index'
   post '/webhooks/event', to: 'unit#event'
   post '/webhooks/answer', to: 'unit#answer'
-  post '/authenticate', to: 'unit#authenticate'
+  post '/record', to: 'unit#record'
   post '/menu-choice', to: 'unit#menu'
 end
